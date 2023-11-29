a4e3a3b2d8bfa322e413b9623e0dfe5ce739e71a
jorge cantillana <cantillanacarrasco@gmail.com>
2023-11-29 18:49:11 -0300

    version mil de pruba github

--- Sender.py
+++ Sender.py
@@ -19,6 +19,8 @@ class Sender:
 
     def send_image(self):
         """ Sends the image data with the header """
+
+        "version 2 GITHUB COPILOT"
         pass
 
     def send_text(self, freqList) -> np.ndarray:
