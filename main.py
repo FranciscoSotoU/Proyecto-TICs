4237c95bd1f96b229627a0cc68d7490fa283918c
jorge cantillana <cantillanacarrasco@gmail.com>
2023-12-15 01:35:51 -0300

    ideal case of 2 senders running OK

--- main.py
+++ main.py
@@ -15,20 +15,38 @@ from scipy.io import wavfile
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    sender = Sender(1000, 5000)
-    sender.load_image("./data/1_20_Imagen1.png") # Image1 20x20 pixels
-    sender.load_text("./data/text.txt") # Text file
+    sender1 = Sender(1000, 5000)
+    sender2 = Sender(6000, 10000)
 
-    audio = sender.send_all_data()
+    sender1.load_image("./data/1_20_Imagen1.png") # Image1 20x20 pixels
+    sender1.load_text("./data/text.txt") # Text file
+    audio1 = sender1.send_all_data()
+
+    sender2.load_image("./data/1_14_Imagen2.png") # Image1 20x20 pixels
+    sender2.load_text("./data/text2.txt") # Text file
+    audio2 = sender2.send_all_data()
+
+    # padding if len(audio1) != len(audio2)
+    if len(audio1) > len(audio2):
+        audio2 = np.pad(audio2, (0, len(audio1) - len(audio2)),'constant')
+    else:
+        audio1 = np.pad(audio1, (0, len(audio2) - len(audio1)),'constant')
+
+    audio = audio1 + audio2
 
     plt.plot(audio)
     plt.show()
 
-    receiver = Receiver(1000, 5000)
-    #fs, audio = wavfile.read("audio_nuevo.wav")
+    receiver1 = Receiver(1000, 5000, 20)
+    receiver2 = Receiver(6000, 10000, 14)
 
-    img, text = receiver.demux_audio(audio)
-    print(text)
-    plt.imshow(img, cmap='gray')
+    img1, text1 = receiver1.demux_audio(audio)
+    img2, text2 = receiver2.demux_audio(audio)
+
+    print(text1)
+    print(text2)
+    plt.imshow(img1, cmap='gray')
+    plt.show()
+    plt.imshow(img2, cmap='gray')
     plt.show()
 
